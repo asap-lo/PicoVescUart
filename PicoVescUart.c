bca6f2f5133e4bc972eb429e7e9636521326c91d
Nicholas Hyder <nic.hyder@gmail.com>
2024-09-14 18:08:43 -0400

    get data printout from VESC

--- PicoVescUart.c
+++ PicoVescUart.c
@@ -2,44 +2,59 @@
  * Made by Jack Lombardo, 2024
  */
 
-#include "pico/stdlib.h"
-#include "hardware/uart.h"
-#include "vesc/bldc_interface_uart.h"
-#include "vesc/bldc_interface.h"
 #include <string.h>
 #include <stdio.h>
+#include "pico/stdlib.h"
+#include "pico/multicore.h"
+#include "hardware/uart.h"
+#include "hardware/irq.h"
+#include "vesc/bldc_interface_uart.h"
+#include "vesc/bldc_interface.h"
 
-#define UART_ID 	uart1
-#define BAUD_RATE 	115200
-#define DATA_BITS 	8
-#define STOP_BITS 	1
-#define PARITY    	UART_PARITY_NONE
+#define UART_ID uart1
+#define BAUD_RATE 115200
+#define DATA_BITS 8
+#define STOP_BITS 1
+#define PARITY UART_PARITY_NONE
 
 // We are using pins 0 and 1, but see the GPIO function select table in the
 // datasheet for information on which other pins can be used.
-#define UART_TX_PIN 0
-#define UART_RX_PIN 1
+#define UART_TX_PIN 8
+#define UART_RX_PIN 9
 
-static void send_packet(unsigned char *data, unsigned int len) {
-	if (len > (PACKET_MAX_PL_LEN + 5)) {
+#define TIME_BETWEEN_REQUESTS_MS	50
+
+// RX interrupt handler
+void on_uart_rx()
+{
+	while (uart_is_readable(UART_ID))
+	{
+		uint8_t ch = uart_getc(UART_ID);
+		bldc_interface_uart_process_byte(ch);
+	}
+}
+
+static void send_packet(unsigned char *data, unsigned int len)
+{
+	if (len > (PACKET_MAX_PL_LEN + 5))
+	{
 		return;
 	}
 
-	// Wait for the previous transmission to finish.
-	uart_tx_wait_blocking(UART_ID);
-
-	// Copy this data to a new buffer in case the provided one is re-used
-	// after this function returns.
 	static uint8_t buffer[PACKET_MAX_PL_LEN + 5];
 	memcpy(buffer, data, len);
 
-	// Send the data over UART
-	uart_write_blocking(UART_ID, buffer, len);
+	for (int i = 0; i < len; i++)
+	{
+		printf("putting char %c\n", data[i]);
+	}
 
+	uart_write_blocking(UART_ID, buffer, len);
 }
 
-void bldc_val_received(mc_values *val) {
-	printf("\r\n");
+void bldc_val_received(mc_values *val)
+{
+	printf("\rGOT SOMETHING?\n");
 	printf("Input voltage: %.2f V\r\n", val->v_in);
 	printf("Temp:          %.2f degC\r\n", val->temp_mos);
 	printf("Current motor: %.2f A\r\n", val->current_motor);
@@ -52,32 +67,50 @@ void bldc_val_received(mc_values *val) {
 	printf("Wh Regen:      %.4f Wh\r\n", val->watt_hours_charged);
 	printf("Tacho:         %i counts\r\n", val->tachometer);
 	printf("Tacho ABS:     %i counts\r\n", val->tachometer_abs);
-	printf("Fault Code:    %s\r\n", bldc_interface_fault_to_string(val->fault_code));	
+	printf("Fault Code:    %s\r\n", bldc_interface_fault_to_string(val->fault_code));
 }
 
-void comm_uart_init(void) {
-
+void comm_uart_init(void)
+{
 	uart_init(UART_ID, BAUD_RATE);
+
+	// Set the TX and RX pins by using the function select on the GPIO
+	// Set datasheet for more information on function select
+	gpio_set_function(UART_TX_PIN, UART_FUNCSEL_NUM(UART_ID, UART_TX_PIN));
+	gpio_set_function(UART_RX_PIN, UART_FUNCSEL_NUM(UART_ID, UART_RX_PIN));
+
+	// Set our data format
+	uart_set_format(UART_ID, DATA_BITS, STOP_BITS, PARITY);
+
+	// Turn off FIFO's - we want to do this character by character
+	uart_set_fifo_enabled(UART_ID, false);
+
+	// Set up a RX interrupt
+	// We need to set up the handler first
+	// Select correct interrupt for the UART we are using
+	int UART_IRQ = UART_ID == uart0 ? UART0_IRQ : UART1_IRQ;
+
+	// And set up and enable the interrupt handlers
+	irq_set_exclusive_handler(UART_IRQ, on_uart_rx);
+	irq_set_enabled(UART_IRQ, true);
+
+	// Now enable the UART to send interrupts - RX only
+	uart_set_irq_enables(UART_ID, true, false);
 
 	bldc_interface_uart_init(send_packet);
 	// init reader callback
+	// bldc_interface_set_sim_values_func()
 	bldc_interface_set_rx_value_func(bldc_val_received);
 }
 
-void read_packet(void) {
-	for (int i = 0; i < PACKET_MAX_PL_LEN; i++) {
-		bldc_interface_uart_process_byte(uart_getc(UART_ID));
-	}
-
-}
-
-int main() {
-	printf("HELLO THERE!");
-    // Set up our UART with a basic baud rate.
+int main()
+{
+	stdio_init_all();
 	comm_uart_init();
-	printf("comm_uart_init ran");
-	bldc_interface_get_values();
-	printf("bldc_interface_get_values started");
-	read_packet();
-    
+	while (true)
+	{
+		printf("attempting to get values loop:\n");
+		bldc_interface_get_values();
+		sleep_ms(TIME_BETWEEN_REQUESTS_MS);
+	}
 }
